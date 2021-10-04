0e13f9daa02a3cb7ca94db08ac9e4f99fb39a7f1
Neil Barazarte <ezebarazarte@gmail.com>
2021-10-04 14:41:58 -0400

    Primer commit de la v2

---
+++ formularioCustomizer.php
@@ -0,0 +1,48 @@
+<!-- Bootstrap CSS -->
+
+<!--
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+-->
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+<script src="https://kit.fontawesome.com/3be368b9d8.js" crossorigin="anonymous"></script>
+
+<style type="text/css">
+  
+  .woocommerce-product-gallery,
+  .price {
+    display:none;
+  }
+
+  .woocommerce-Price-amount, 
+  .amount {
+    display:none; 
+  }
+
+
+  .woocommerce #content div.product div.summary, 
+  .woocommerce div.product div.summary, 
+  .woocommerce-page #content div.product div.summary, 
+  .woocommerce-page div.product div.summary {
+    /*float: left;*/
+  }
+
+  .product_title, h1.entry-title{
+      display:none;
+  }
+
+.woocommerce td.product-name .wc-item-meta p, .woocommerce td.product-name .wc-item-meta:last-child, .woocommerce td.product-name dl.variation p, .woocommerce td.product-name dl.variation:last-child {
+    /* margin-bottom: 0; */
+}  
+
+</style>
+
+<div class="bsnamespace">
+
+<?php
+  require('formulario/formulario.php');
+?>
+
+</div>
