813911aea18106a0ba18008704de23fd9ae14c1c
Neil Barazarte <ezebarazarte@gmail.com>
2021-10-05 14:01:21 -0400

    Paleta de colores

--- formulario/formaContorno.php
+++ formulario/formaContorno.php
@@ -218,51 +218,53 @@
             <div class="cc-selector">
               <center>
 
-				<figure class="figure">
-
-                <input type="radio" name="colores" value="blanco" id="blancoColor"  onchange="deshabiltarBotonCart()"> 
-                <label class="drinkcard-cc-colores blancoColor" for="blancoColor"></label> 
-
-                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">BLANCO</figcaption>
-
-              </figure>
               <figure class="figure">
 
-                <input type="radio" name="colores" value="amarillo" id="amarilloColor"  onchange="deshabiltarBotonCart()"> 
-                <label class="drinkcard-cc-colores amarilloColor" for="amarilloColor"></label> 
+                <input  type="radio" name="colores" value="rojo" id="rojoColor" checked onchange="deshabiltarBotonCart()">
+                <label class="drinkcard-cc-colores rojoColor" for="rojoColor"></label>
 
-                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">AMARILLO</figcaption>
+                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">ROJO</figcaption>
 
               </figure>
-			  
-			  <figure class="figure">
 
-                <input type="radio" name="colores" value="durazno" id="duraznoColor" checked onchange="deshabiltarBotonCart()"> 
+              <figure class="figure">
+
+                <input type="radio" name="colores" value="durazno" id="duraznoColor" onchange="deshabiltarBotonCart()"> 
                 <label class="drinkcard-cc-colores duraznoColor" for="duraznoColor"></label> 
 
                 <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">DURAZNO</figcaption>
 
               </figure>
 
-			  <figure class="figure">
+              <figure class="figure">
 
-                <input type="radio" name="colores" value="naranja" id="naranjaColor" checked onchange="deshabiltarBotonCart()"> 
-                <label class="drinkcard-cc-colores naranjaColor" for="naranjaColor"></label> 
+                <input type="radio" name="colores" value="ambar" id="ambarColor" onchange="deshabiltarBotonCart()"> 
+                <label class="drinkcard-cc-colores ambarColor" for="ambarColor"></label> 
 
-                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">NARANJA</figcaption>
+                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">AMBAR</figcaption>
+
+              </figure>              
+
+				      <figure class="figure">
+
+                <input type="radio" name="colores" value="limon" id="limonColor" onchange="deshabiltarBotonCart()"> 
+                <label class="drinkcard-cc-colores limonColor" for="limonColor"></label> 
+
+                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">LIMÓN</figcaption>
+
+              </figure>
+     
+
+              <figure class="figure">
+
+                <input type="radio" name="colores" value="amarillo" id="amarilloColor" onchange="deshabiltarBotonCart()"> 
+                <label class="drinkcard-cc-colores amarilloColor" for="amarilloColor"></label> 
+
+                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">AMARILLO</figcaption>
 
               </figure>
 			  
-			  <figure class="figure">
-
-                <input  type="radio" name="colores" value="rojo" id="rojoColor" onchange="deshabiltarBotonCart()">
-                <label class="drinkcard-cc-colores rojoColor" for="rojoColor"></label>
-
-                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">ROJO</figcaption>
-
-              </figure>
-			  
-			  <figure class="figure">
+             <figure class="figure">
 
                 <input  type="radio" name="colores" value="verde" id="verdeColor" onchange="deshabiltarBotonCart()">
                 <label class="drinkcard-cc-colores verdeColor" for="verdeColor"></label>
@@ -280,25 +282,6 @@
 
               </figure>
 
-              <figure class="figure">
-
-                <input  type="radio" name="colores" value="turquesa" id="turquesaColor" onchange="deshabiltarBotonCart()">
-                <label class="drinkcard-cc-colores turquesaColor" for="turquesaColor"></label>
-
-                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">TURQUESA</figcaption>
-
-              </figure>
-              
-
-              <figure class="figure">
-
-                <input  type="radio" name="colores" value="fucsia" id="fucsiaColor" onchange="deshabiltarBotonCart()"> 
-                <label class="drinkcard-cc-colores fucsiaColor" for="fucsiaColor"></label> 
-
-                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">FUCSIA</figcaption>
-
-              </figure>
-              
 
               <figure class="figure">
 
@@ -308,6 +291,53 @@
                 <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">MORADO</figcaption>
 
               </figure>
+
+
+              <figure class="figure">
+
+                <input  type="radio" name="colores" value="turquesa" id="turquesaColor" onchange="deshabiltarBotonCart()">
+                <label class="drinkcard-cc-colores turquesaColor" for="turquesaColor"></label>
+
+                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">TURQUESA</figcaption>
+
+              </figure>
+
+              <figure class="figure">
+
+                <input  type="radio" name="colores" value="rosa" id="rosaColor" onchange="deshabiltarBotonCart()"> 
+                <label class="drinkcard-cc-colores rosaColor" for="rosaColor"></label> 
+
+                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">ROSA</figcaption>
+
+              </figure>
+
+			        <figure class="figure">
+
+                <input type="radio" name="colores" value="blanco" id="blancoColor" onchange="deshabiltarBotonCart()"> 
+                <label class="drinkcard-cc-colores blancoColor" for="blancoColor"></label> 
+
+                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">BLANCO</figcaption>
+
+              </figure>			 
+			  
+              <figure class="figure">
+
+                <input type="radio" name="colores" value="calido" id="calidoColor" onchange="deshabiltarBotonCart()"> 
+                <label class="drinkcard-cc-colores calidoColor" for="calidoColor"></label> 
+
+                <figcaption class="figure-caption text-center" style="font-size: 8px; color: #000000; position: relative; top: -21px; left: 5px;">CÁLIDO</figcaption>
+
+              </figure>
+
+
+
+
+              
+
+
+              
+
+
 
 
               </center>
